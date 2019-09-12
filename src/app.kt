25af6cbfe7d957a8ce532a4661676e77cb53a8c3
AlekseiCherepanov <mrneumann888@gmail.com>
2019-09-13 02:01:49 +0300

    add comments

--- src/app.kt
+++ src/app.kt
@@ -1,11 +1,12 @@
 import java.lang.Exception
 import java.util.*
+
 val variables: MutableMap<String, Int> = mutableMapOf<String, Int>()
 
 fun main() {
     var newExpression: String?
     while(true) {
-        print(">>>")
+        print(">>> ")
         newExpression = readLine()
         if(newExpression == null || newExpression == ""){
             continue
@@ -13,10 +14,12 @@ fun main() {
         val cutExp = newExpression.split("=")
         try {
             if (cutExp[0] == newExpression) {
+                //обычное выражение без присваивания - считаем
                 println(countPolishExpr(infixToPolishNotation(newExpression)))
             } else {
                 val cutExp2 = cutExp[0].split("let ")
                 if (cutExp2[0] != cutExp[0]) {
+                    //создаем переменную и считаем ее значение
                     variables[cutExp2[1].trimEnd()] = countPolishExpr(infixToPolishNotation(cutExp[1]))
                 }
             }
@@ -26,29 +29,30 @@ fun main() {
     }
 }
 
+//калькулятор для выражений в польской записи
 fun countPolishExpr(stack:LinkedList<Any>):Int{
-    var countStack = LinkedList<Int>()
+    val countStack = LinkedList<Int>()
     while(stack.isNotEmpty()){
-        var top = stack.pop()
+        val top = stack.pop()
         when (top) {
             '+' -> {
-                var a = countStack.pop()
-                var b = countStack.pop()
+                val a = countStack.pop()
+                val b = countStack.pop()
                 countStack.push(a+b)
             }
             '-' -> {
-                var a = countStack.pop()
-                var b = countStack.pop()
+                val a = countStack.pop()
+                val b = countStack.pop()
                 countStack.push(b-a)
             }
             '*' -> {
-                var a = countStack.pop()
-                var b = countStack.pop()
+                val a = countStack.pop()
+                val b = countStack.pop()
                 countStack.push(a*b)
             }
             '/' -> {
-                var a = countStack.pop()
-                var b = countStack.pop()
+                val a = countStack.pop()
+                val b = countStack.pop()
                 countStack.push(b/a)
             }
             else -> {
@@ -59,13 +63,14 @@ fun countPolishExpr(stack:LinkedList<Any>):Int{
     return countStack.first
 }
 
+//инфиксное представление to польская запись + подставление переменнных
 fun infixToPolishNotation(expr: String):LinkedList<Any>{
     val outStack = LinkedList<Any>()
     val operationStack = LinkedList<Char>()
     var stringNumber = ""
     var stringVariable = ""
-    fun check(){
 
+    fun check(){
         if(stringVariable != ""){
             val variableValue = variables[stringVariable]
 
@@ -80,6 +85,7 @@ fun infixToPolishNotation(expr: String):LinkedList<Any>{
             stringNumber = ""
         }
     }
+
     fun getPriority(topStack:Char, operation:Char): Boolean{
         if(topStack == '*' || topStack == '/') {
             return true
@@ -88,6 +94,7 @@ fun infixToPolishNotation(expr: String):LinkedList<Any>{
         }
         return false
     }
+
     loop@ for(i:Char in expr){
         when (i) {
             ' ' -> {
@@ -116,12 +123,17 @@ fun infixToPolishNotation(expr: String):LinkedList<Any>{
             }
             ')' -> {
                 check()
+                if(operationStack.isEmpty()){
+                    throw Exception("Wrong input! Check brackets!")
+                }
                 var operation = operationStack.pop()
                 while(operation != '('){
                     outStack.push(operation)
+                    if(operationStack.isEmpty()){
+                        throw Exception("Wrong input! Check brackets!")
+                    }
                     operation = operationStack.pop()
                 }
-                //TODO: ERROR whith bracets
             }
             '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' -> {
                 stringNumber += i
@@ -140,10 +152,10 @@ fun infixToPolishNotation(expr: String):LinkedList<Any>{
 //        print(i)
 //    }
 //    println()
-
     return outStack
 }
 
+// examples for check
 // 3 + 4 * 2 / (1 - 5)
 // (1+2)*4+3
 // let v = (1+2)*4+3
